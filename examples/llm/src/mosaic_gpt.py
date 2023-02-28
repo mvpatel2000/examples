5e0b99c08b4554f0eb39ea518d025b08a9399546
Mihir Patel <mihir.v.patel7@gmail.com>
2023-02-27 17:08:08 -0800

    add print

--- examples/llm/src/mosaic_gpt.py
+++ examples/llm/src/mosaic_gpt.py
@@ -565,6 +565,7 @@ class ComposerMosaicGPT(ComposerModel):
             self.eval_metrics = evaluator_metrics
 
     def flops_per_batch(self, batch):
+        print(batch['input_ids'].shape[0])
         if self.__num_fwd_flops:
             return self.__num_fwd_flops * 3 * batch['input_ids'].shape[0]
         n_params = sum(p.numel() for p in self.parameters())
